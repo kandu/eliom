a3b643a553a30fae21f16475808e48adfeb70830
Vasilis Papavasileiou <git@vasilis.airpost.net>
2017-03-31 16:42:20 +0200

    Switch to Cohttp

--- src/lib/eliom_comet.server.ml
+++ src/lib/eliom_comet.server.ml
@@ -490,8 +490,10 @@ end = struct
     signal_update handler
 
   let wait_closed_connection () =
-    let ri = Eliom_request_info.get_ri () in
-    let%lwt () = Ocsigen_extensions.Ocsigen_request_info.connection_closed ri in
+    let%lwt () =
+      Ocsigen_request.connection_closed
+        (Eliom_request_info.get_ri ())
+    in
     Lwt.fail Connection_closed
 
   (* register the service handler.hd_service *)
